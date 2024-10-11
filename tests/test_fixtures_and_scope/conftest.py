d98882ba0344a9663a57f8d412d0255e86d7aa59
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-10-11 21:00:34 +0300

    Created temporary file for testing csv files.

--- tests/test_fixtures_and_scope/conftest.py
+++ tests/test_fixtures_and_scope/conftest.py
@@ -1,19 +1,39 @@
+import csv
+import os
+import shutil
+
 import pytest
-from starlette.templating import pass_context
 
-
-@pytest.fixture(scope='session')
-def single_joke_csv_filepath(tmp_path):
+@pytest.fixture(scope="session")
+def single_joke_csv_filepath():
     #a fixture that returns the path to a temporary csv file containing a single joke 
     # (the fixture may create the file or the file can be created in another fixture)
     
-    temp_file = tmp_path / 'temp_file.csv'
+    #Luodaan directory juureen nimeltään tmp-test-resources
+    tmp_test_resources = os.path.join(os.getcwd(), "tmp-test-resources")
+
+    #Varmistetaan, että meidän directory on luotu
+    os.makedirs(tmp_test_resources, exist_ok=True)
     
-    temp_file.write_text("Apple is designing a new automatic car., But they're having trouble installing Windows!")
+    #Luodaan csv - tiedosto
+    temp_jokes_csv = os.path.join(tmp_test_resources, "temp_jokes.csv")
     
-    return temp_file.absolute()
+    #Kirjoitetaan csv-tiedostoon yksi esimerkkivitsi
+    with open(temp_jokes_csv, 'w', newline='') as jokes_file:
+        writer = csv.writer(jokes_file)
+        writer.writerow(["setup", "punchline"])
+        writer.writerow(["Did you hear about the kidnapping at school?", "It’s fine, he woke up."])
+    
+    yield temp_jokes_csv
+    shutil.rmtree(tmp_test_resources)
 
 
-@pytest.fixture(scope='session')
+# single_joke — a fixture that returns the joke from the temporary single joke csv file
+@pytest.fixture()
 def single_joke(single_joke_csv_filepath):
-    return str(single_joke_csv_filepath)
+    with open(single_joke_csv_filepath, 'r', newline='') as jokes_file:
+        reader = csv.reader(jokes_file)
+        contents = list(reader)
+        
+        return contents[1]
+
