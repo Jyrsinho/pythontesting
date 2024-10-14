bb361f7e080df9d5a63e49a1dd79d4696f590f23
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-10-14 18:40:53 +0300

    Fast API- testing

--- tests/integration/test_routes.py
+++ tests/integration/test_routes.py
@@ -8,19 +8,15 @@ app = FastAPI()
 
 client = TestClient
 
+
 #/joke/random - returns a random joke
 def test_random_should_return_a_random_joke():
     # For the /joke/random endpoint, it is enough to check that the server returns a joke in the correct format. 
     response = client.get('/joke/random')
     assert response.status_code == 200
-    joke = response.json()
-    assert 'id' in joke
-    assert 'setup' in joke
-    assert 'punchline' in joke
     
     
-
-
+    
 
 #/joke/{id} - returns a joke with the given id
 #/joke/all - returns a list of all jokes
--- tests/test_fixtures_and_scope/test_conftest.py
+++
@@ -1,5 +0,0 @@
-import pytest
-
-def test_print_joke_csv_filepath(single_joke_csv_filepath):
-    print (str(single_joke_csv_filepath))
-    
