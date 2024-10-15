ef6fa92ad488cd21da8faf4cce0abdd60642af17
Jyri Huhtala <jyrgenhuhtala@gmail.com>
2024-10-15 15:27:07 +0300

    test for returning a joke with given id

--- tests/integration/test_routes.py
+++ tests/integration/test_routes.py
@@ -5,32 +5,40 @@ from main import app, Joke
 
 client = TestClient(app)
 
+@patch('main.find_jokes')  # Mock the find_jokes function
+def test_should_return_random_joke(mock_find_jokes, test_jokes):
+    # Mock the find_jokes function to return the test_jokes fixture
+    mock_find_jokes.return_value = test_jokes
 
-# Mock data
-mock_jokes = [
-    Joke(id=1, setup="Why did the chicken cross the road?", punchline="To get to the other side."),
-    Joke(id=2, setup="Why don't skeletons fight?", punchline="They don't have the guts.")
-]
-
-
-@patch('main.find_jokes', return_value=mock_jokes)  # Mock the find_jokes function
-def test_should_return_random_joke(mock_find_jokes):
     response = client.get('/joke/random')
     assert response.status_code == 200
 
     joke = response.json()
 
-    # Check that the joke contains the necessary fields
+    # Tarkistetaan, että vitsillä on oikeat kentät.
     assert 'id' in joke
     assert 'setup' in joke
     assert 'punchline' in joke
 
-    # Check that the joke is one of the jokes in mock_jokes
-    assert joke['id'] in [1, 2]
-    assert any(j['setup'] == joke['setup'] and j['punchline'] == joke['punchline'] for j in [
-        {"id": 1, "setup": "Why did the chicken cross the road?", "punchline": "To get to the other side."},
-        {"id": 2, "setup": "Why don't skeletons fight?", "punchline": "They don't have the guts."}
-    ])
-    
-def test_should_return_a_joke_with_given_id()
+    # Tarkistetaan, että vitsi on yksi testvitseistä
+    assert joke['id'] in [j['id'] for j in test_jokes]
+    assert any(j['setup'] == joke['setup'] and j['punchline'] == joke['punchline'] for j in test_jokes)
+    print(joke)
 
+
+#/joke/{id} - returns a joke with the given id
+@patch('main.find_jokes')
+def test_should_return_joke_with_given_id(mock_find_jokes, test_jokes):
+    mock_find_jokes.return_value = test_jokes
+    
+    response = client.get('/joke/2')
+    assert response.status_code == 200
+    
+    joke = response.json()
+    
+    #Tarkistetaan, että tuli oikea vitsi
+    assert joke['id'] == 2
+    assert joke['setup'] == 'How do you organize a space party?'
+    assert joke['punchline'] == 'You planet'
+    
+    
